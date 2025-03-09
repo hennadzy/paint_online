85e738caed83923150589ae2303e678baa176b72
hennadz <hennadz@yandex.by>
2025-03-09 18:09:14 +0300

    Локальная отрисовка

--- client/src/tools/Brush.js
+++ client/src/tools/Brush.js
@@ -1,5 +1,3 @@
-import Tool from "./Tool";
-
 export default class Brush extends Tool {
     constructor(canvas, socket, id) {
         super(canvas, socket, id);
@@ -8,25 +6,16 @@ export default class Brush extends Tool {
         this.listen();
     }
 
-    listen() {
-        // Мышь
-        this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
-        this.canvas.onmousedown = this.mouseDownHandler.bind(this);
-        this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+    // ... (тут без изменений ваш существующий код listen(), mouseUpHandler и пр.)
 
-        // Сенсор
-        this.canvas.addEventListener('touchstart', this.touchStartHandler.bind(this));
-        this.canvas.addEventListener('touchmove', this.touchMoveHandler.bind(this));
-        this.canvas.addEventListener('touchend', this.touchEndHandler.bind(this));
-        this.canvas.addEventListener('touchcancel', this.touchEndHandler.bind(this)); // На всякий случай
+    draw(x, y) {
+        this.sendDrawData(x, y, false);
     }
 
-     // Общая функция рисования
-     sendDrawData(x, y, isStart = false) {
+    sendDrawData(x, y, isStart = false) {
         const lineWidth = this.ctx.lineWidth;
         const strokeStyle = this.ctx.strokeStyle;
-    
-        // добавляем проверку сокета
+
         if (this.socket) {
             this.socket.send(JSON.stringify({
                 method: 'draw',
@@ -39,74 +28,18 @@ export default class Brush extends Tool {
                     strokeStyle,
                     isStart
                 }
-            }))
+            }));
         }
-    
-        // И тут же выполняем локальную отрисовку
+
         Brush.staticDraw(this.ctx, x, y, lineWidth, strokeStyle, isStart);
     }
 
-    // --- Обработчики событий мыши ---
-    mouseUpHandler(e) {
-        this.mouseDown = false;
-        this.socket.send(JSON.stringify({
-            method: 'draw',
-            id: this.id,
-            figure: {
-                type: 'finish',
-            }
-        }))
-    }
-
-    mouseDownHandler(e) {
-        this.mouseDown = true;
-        this.ctx.beginPath();
-        this.ctx.moveTo(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
-       this.sendDrawData(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop, true);
-    }
-
-    mouseMoveHandler(e) {
-        if (this.mouseDown) {
-             this.sendDrawData(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop)
-        }
-    }
-
-    // --- Обработчики сенсорных событий ---
-
-    touchStartHandler(e) {
-        this.mouseDown = true;
-        this.ctx.beginPath();
-        this.ctx.moveTo(e.touches[0].pageX - e.target.offsetLeft, e.touches[0].pageY - e.target.offsetTop);
-         this.sendDrawData(e.touches[0].pageX - e.target.offsetLeft, e.touches[0].pageY - e.target.offsetTop, true)
-    }
-
-
-    touchMoveHandler(e) {
-        e.preventDefault(); // Предотвращаем прокрутку
-        if (this.mouseDown) {
-         this.sendDrawData(e.touches[0].pageX - e.target.offsetLeft, e.touches[0].pageY - e.target.offsetTop)
-        }
-    }
-
-
-    touchEndHandler() {
-      this.mouseDown = false;
-        this.socket.send(JSON.stringify({
-            method: 'draw',
-            id: this.id,
-            figure: {
-                type: 'finish',
-            }
-        }))
-    }
-
-    // Статический метод для отрисовки
     static staticDraw(ctx, x, y, lineWidth, strokeStyle, isStart = false) {
-        if(isStart){
+        if (isStart) {
             ctx.beginPath();
-            ctx.moveTo(x,y);
+            ctx.moveTo(x, y);
         }
-         ctx.lineTo(x, y);
+        ctx.lineTo(x, y);
         ctx.lineWidth = lineWidth;
         ctx.strokeStyle = strokeStyle;
         ctx.stroke();
