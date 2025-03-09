f9b0e65e9022d3fd72b573e27edfcb9c8343a761
hennadz <hennadz@yandex.by>
2025-03-09 15:17:18 +0300

    Улучшение сенсорного взаимодействия как есть

--- client/src/tools/Rect.js
+++ client/src/tools/Rect.js
@@ -4,18 +4,23 @@ export default class Rect extends Tool {
     constructor(canvas, socket, id) {
         super(canvas, socket, id);
         this.mouseDown = false;
-
         this.listen();
     }
 
     listen() {
-        this.canvas.addEventListener('mousedown', this.mouseDownHandler.bind(this));
-        this.canvas.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
-        this.canvas.addEventListener('mouseup', this.mouseUpHandler.bind(this));
+        this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+        this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+        this.canvas.onmouseup = this.mouseUpHandler.bind(this);
 
-        this.canvas.addEventListener('touchstart', this.touchStartHandler.bind(this),{passive:false});
-        this.canvas.addEventListener('touchmove', this.touchMoveHandler.bind(this),{passive:false});
-        this.canvas.addEventListener('touchend', this.touchEndHandler.bind(this),{passive:false});
+        // сенсорные события
+        this.canvas.addEventListener('touchstart', this.touchStartHandler.bind(this), { passive: false });
+        this.canvas.addEventListener('touchmove', this.touchMoveHandler.bind(this), { passive: false });
+        this.canvas.addEventListener('touchend', this.touchEndHandler.bind(this), { passive: false });
+    }
+
+    mouseUpHandler(e) {
+        this.mouseDown = false;
+        this.sendRect();
     }
 
     mouseDownHandler(e) {
@@ -27,19 +32,18 @@ export default class Rect extends Tool {
     }
 
     mouseMoveHandler(e) {
-        if(this.mouseDown) {
+        if (this.mouseDown) {
             const rect = this.canvas.getBoundingClientRect();
-            this.currentX = e.clientX - rect.left;
-            this.currentY = e.clientY - rect.top;
-            this.draw(this.startX, this.startY, this.currentX - this.startX, this.currentY - this.startY);
+            const currentX = e.clientX - rect.left;
+            const currentY = e.clientY - rect.top;
+            this.width = currentX - this.startX;
+            this.height = currentY - this.startY;
+            this.draw(this.startX, this.startY, this.width, this.height);
         }
     }
-    mouseUpHandler() {
-        this.mouseDown = false;
-        this.sendRect();
-    }
 
-    touchStartHandler(e){
+    // Сенсорные события
+    touchStartHandler(e) {
         e.preventDefault();
         this.mouseDown = true;
         const rect = this.canvas.getBoundingClientRect();
@@ -48,59 +52,61 @@ export default class Rect extends Tool {
         this.saved = this.canvas.toDataURL();
     }
 
-    touchMoveHandler(e){
+    touchMoveHandler(e) {
         e.preventDefault();
-        if(this.mouseDown){
+        if (this.mouseDown) {
             const rect = this.canvas.getBoundingClientRect();
-            this.currentX = e.touches[0].clientX - rect.left;
-            this.currentY = e.touches[0].clientY - rect.top;
-            this.draw(this.startX, this.startY, this.currentX - this.startX, this.currentY - this.startY);
+            const currentX = e.touches[0].clientX - rect.left;
+            const currentY = e.touches[0].clientY - rect.top;
+            this.width = currentX - this.startX;
+            this.height = currentY - this.startY;
+            this.draw(this.startX, this.startY, this.width, this.height);
         }
     }
 
-    touchEndHandler(e){
+    touchEndHandler(e) {
         e.preventDefault();
         this.mouseDown = false;
         this.sendRect();
     }
 
-    sendRect(){
+    sendRect() {
         this.socket.send(JSON.stringify({
             method: 'draw',
             id: this.id,
-            figure:{
+            figure: {
                 type: 'rect',
                 x: this.startX,
                 y: this.startY,
-                width: this.currentX - this.startX,
-                height: this.currentY - this.startY,
-                fillStyle: this.ctx.fillStyle,
-                strokeStyle: this.ctx.strokeStyle,
+                width: this.width,
+                height: this.height,
+                color: this.ctx.fillStyle,
                 lineWidth: this.ctx.lineWidth,
+                strokeStyle: this.ctx.strokeStyle,
             }
         }));
-        this.socket.send(JSON.stringify({method: 'draw', id: this.id, figure: {type: 'finish'}}));
+        this.socket.send(JSON.stringify({ method: 'draw', id: this.id, figure: { type: 'finish' } }));
     }
 
-    draw(x,y,w,h){
+    draw(x, y, w, h) {
         const img = new Image();
         img.src = this.saved;
-        img.onload = ()=>{
-            this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-            this.ctx.drawImage(img,0,0,this.canvas.width,this.canvas.height);
+        img.onload = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
             this.ctx.beginPath();
-            this.ctx.rect(x,y,w,h);
+            this.ctx.rect(x, y, w, h);
             this.ctx.fill();
             this.ctx.stroke();
-        }
+        };
     }
 
-    static staticDraw(ctx, x, y, w, h, lineWidth, strokeStyle, fillStyle){
+    static staticDraw(ctx, x, y, w, h, lineWidth, strokeStyle, color) {
+        ctx.fillStyle = color;
         ctx.lineWidth = lineWidth;
         ctx.strokeStyle = strokeStyle;
-        ctx.fillStyle = fillStyle;
         ctx.beginPath();
-        ctx.rect(x,y,w,h);
+        ctx.rect(x, y, w, h);
         ctx.fill();
         ctx.stroke();
     }
