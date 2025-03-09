721599d4970cc8b3d353436cad1dc3b94a0e6845
hennadz <hennadz@yandex.by>
2025-03-09 15:20:07 +0300

    Улучшение сенсорного взаимодействия как есть

--- client/src/tools/Tool.js
+++ client/src/tools/Tool.js
@@ -1,26 +1,26 @@
 export default class Tool {
     constructor(canvas, socket, id) {
-        this.canvas = canvas;
-        this.socket = socket;
-        this.id = id;
-        this.ctx = canvas.getContext('2d');
-        this.removeAllEventListeners(); // <-- очищаем обработчики при смене любого инструмента
+        this.canvas = canvas
+        this.socket = socket
+        this.id = id
+        this.ctx = canvas.getContext('2d')
+        this.destroyEvents()
     }
 
-    set fillColor(color) { this.ctx.fillStyle = color; }
-    set strokeColor(color) { this.ctx.strokeStyle = color; }
-    set lineWidth(width) { this.ctx.lineWidth = width; }
+    set fillColor(color) {
+        this.ctx.fillStyle = color
+    }
+    set strokeColor(color) {
+        this.ctx.strokeStyle = color
+    }
 
-    removeAllEventListeners() {
-        const clone = this.canvas.cloneNode(true);
-        this.canvas.parentNode.replaceChild(clone, this.canvas);
+    set lineWidth(width) {
+        this.ctx.lineWidth = width
+    }
 
-        // ВАЖНО: обновляем ссылку в глобальном состоянии
-        if (window.toolState) {
-            window.toolState.setCanvas(clone);
-        }
-
-        this.canvas = clone;
-        this.ctx = this.canvas.getContext('2d');
+    destroyEvents() {
+        this.canvas.onmousemove = null
+        this.canvas.onmousedown = null
+        this.canvas.onmouseup = null
     }
 }
