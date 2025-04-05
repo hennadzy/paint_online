e3ba86221eee582dbf18121a0c7fbf92e44ffa80
hennadz <hennadz@yandex.by>
2025-04-05 22:55:25 +0300

    Локальная отрисовка

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -65,20 +65,23 @@ const Canvas = observer(() => {
         );
       };
 
-     socket.onmessage = (event) => {
-    const msg = JSON.parse(event.data);
-
-    // Фильтрация сообщения рисующего пользователя
-    if (msg.id === canvasState.sessionid) return;
-
-    switch (msg.method) {
-        case "draw":
-            drawHandler(msg);
+      socket.onmessage = (event) => {
+        const msg = JSON.parse(event.data);
+  
+        // Если сообщение от самого рисующего, пропускаем обработку
+        if (msg.username === canvasState.username) return;
+  
+        switch (msg.method) {
+          case "connection":
+            setMessages((prevMessages) => [...prevMessages, `${msg.username} вошел в комнату`]);
             break;
-        default:
+          case "draw":
+            drawHandler(msg); // вызываем обработчик рисования
             break;
-    }
-};
+          default:
+            break;
+        }
+      };
     }
   }, [canvasState.username, params.id]);
 
