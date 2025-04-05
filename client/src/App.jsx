eefbafef162417b23568a4b7b2c753b2ace3e905
hennadz <hennadz@yandex.by>
2025-04-05 17:40:39 +0300

    Локальная отрисовка

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,70 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Toolbar from './components/Toolbar';
-import SettingBar from './components/SettingBar';
-import Canvas from './components/Canvas';
+import "./styles/app.scss"
+import SettingBar from "./components/SettingBar";
+import Toolbar from "./components/Toolbar";
+import Canvas from "./components/Canvas";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 const App = () => {
-    const generateUniquePath = () => {
-        return (+new Date()).toString(16);
-    };
-
     return (
-        <Router>
-            <Routes>
-                <Route path='/:id' element={
-                    <>
-                        <Toolbar />
-                        <SettingBar />
-                        <Canvas />
-                    </>
-                } />
+        <Routes>
+            <Route path='/' element={
+                <>
+                    <Toolbar />
+                    <SettingBar />
+                    <Canvas />
+                </>
+            } />
+            <Route path='/:id'>
+                <Route path="home" />
+            </Route>
+        </Routes>
+        // <Routes>
+        //     <Route path='/:id' element={
+        //         <>
+        //             <Toolbar />
+        //             <SettingBar />
+        //             <Canvas />
+        //         </>
+        //     } />
+        //     <Route path='/' element={
+        //         <Navigate to={`f${(+new Date).toString(16)}`} />
+        //     }
+        //     />
+        // </Routes>
 
-                <Route path='/' element={<Navigate to={`/${generateUniquePath()}`} replace />} />
-            </Routes>
-        </Router>
+
     );
 };
 
-export default App;
+// export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Toolbar from './components/Toolbar';
+// import SettingBar from './components/SettingBar';
+// import Canvas from './components/Canvas';
+
+// const App = () => {
+//     const generateUniquePath = () => {
+//         return (+new Date()).toString(16);
+//     };
+
+//     return (
+//         <Router>
+//             <Routes>
+//                 <Route path='/:id' element={
+//                     <>
+//                         <Toolbar />
+//                         <SettingBar />
+//                         <Canvas />
+//                     </>
+//                 } />
+
+//                 <Route path='/' element={<Navigate to={`/${generateUniquePath()}`} replace />} />
+//             </Routes>
+//         </Router>
+//     );
+// };
+
+// export default App;
