e35fb769262e773ef3283227f7cb8a452edb24bd
hennadz <hennadz@yandex.by>
2025-04-05 17:14:46 +0300

    Локальная отрисовка

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,35 +3,22 @@ import "./styles/app.scss"
 import SettingBar from "./components/SettingBar";
 import Toolbar from "./components/Toolbar";
 import Canvas from "./components/Canvas";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <Routes>
-        <Route path='/' element={
-          <>
-            <Toolbar />
-            <SettingBar />
-            <Canvas />
-          </>
-        } />
-        <Route path='/:id' element={<Canvas />} />
-      </Routes>
-        // <Routes>
-        //     <Route path='/:id' element={
-        //         <>
-        //             <Toolbar />
-        //             <SettingBar />
-        //             <Canvas />
-        //         </>
-        //     } />
-        //     <Route path='/' element={
-        //         <Navigate to={`f${(+new Date).toString(16)}`} />
-        //     }
-        //     />
-        // </Routes>
-
-
+        <Router>
+            <Routes>
+                <Route path='/' element={
+                    <>
+                        <Toolbar />
+                        <SettingBar />
+                        <Canvas />
+                    </>
+                } />
+                <Route path='/:id' element={<Canvas />} /> {/* Замените на компонент, который рендерит что-то для /:id */}
+            </Routes>
+        </Router>
     );
 };
 
