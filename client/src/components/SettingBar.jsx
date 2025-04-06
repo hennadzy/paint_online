546592897ef7dda83f1592e8ee2787fcd14268d3
hennadz <hennadz@yandex.by>
2025-04-06 17:32:49 +0300

    Изменение размера мобильной версии

--- client/src/components/SettingBar.jsx
+++ client/src/components/SettingBar.jsx
@@ -4,14 +4,17 @@ import toolState from "../store/toolState";
 const SettingBar = () => {
     return (
         <div className="setting-bar">
-            <label htmlFor="line-width">Толщина линии</label>
+            <label htmlFor="line-width">Толщина линии:</label>
             <input
                 onChange={e => toolState.setLineWidth(e.target.value)}
                 style={{margin: '0 10px'}}
                 id="line-width"
-                type="number" defaultValue={1} min={1} max={50}/>
-            {/* <label htmlFor="stroke-color">Цвет обводки</label>
-            <input onChange={e => toolState.setStrokeColor(e.target.value)} id="stroke-color" type="color"/> */}
+                type="range"
+                defaultValue={1}
+                min={1}
+                max={50} // Лимит толщины линии от 1 до 50 пикселей
+            />
+            <span style={{marginLeft: '10px'}}> {toolState.tool?.lineWidth || 1}px </span> {/* Показываем текущую толщину линии */}
         </div>
     );
 };
