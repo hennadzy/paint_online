c17ed2e33fd01537df90ff5605d0946ccbfa0a9f
hennadz <hennadz@yandex.by>
2025-04-06 15:15:34 +0300

    Локальная отрисовка

--- server/index.js
+++ server/index.js
@@ -61,8 +61,8 @@ const broadcastConnection = (ws, msg) => {
     aWss.clients.forEach(client => {
         if (client.id === msg.id) {
             // Если в сообщении отсутствует username, подставляем сохранённое значение
-            if (!msg.username) {
-                msg.username = ws.username;
+            if (client.id === msg.id && client.username !== msg.username) {
+                client.send(JSON.stringify(msg));
             }
             client.send(JSON.stringify(msg));
         }
