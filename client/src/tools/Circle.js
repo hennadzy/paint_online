5a4c2204098a07cc592edeb4d3f9c0d00573fc76
hennadz <hennadz@yandex.by>
2025-04-06 13:39:07 +0300

    Локальная отрисовка

--- client/src/tools/Circle.js
+++ client/src/tools/Circle.js
@@ -1,171 +1,153 @@
 import Tool from "./Tool";
+import toolState from "../store/toolState";
 
 export default class Circle extends Tool {
-    constructor(canvas, socket, id) {
-        super(canvas, socket, id);
-        this.mouseDown = false;
-        this.startX = 0;
-        this.startY = 0;
-        this.r = 0;
-        this.saved = "";
-        this.destroyEvents();
-        this.listen();
+  constructor(canvas, socket, id, username) {
+    super(canvas, socket, id, username);
+    this.fillColor = toolState.fillColor || this.ctx.strokeStyle;
+    this.mouseDown = false;
+    this._touchStartHandler = this.touchStartHandler.bind(this);
+    this._touchMoveHandler = this.touchMoveHandler.bind(this);
+    this._touchEndHandler = this.touchEndHandler.bind(this);
+    this.destroyEvents();
+    this.listen();
+  }
+
+  destroyEvents() {
+    this.canvas.onmousedown = null;
+    this.canvas.onmousemove = null;
+    this.canvas.onmouseup = null;
+    this.canvas.removeEventListener("touchstart", this._touchStartHandler);
+    this.canvas.removeEventListener("touchmove", this._touchMoveHandler);
+    this.canvas.removeEventListener("touchend", this._touchEndHandler);
+  }
+
+  listen() {
+    this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+    this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+    this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+    this.canvas.addEventListener("touchstart", this._touchStartHandler, { passive: false });
+    this.canvas.addEventListener("touchmove", this._touchMoveHandler, { passive: false });
+    this.canvas.addEventListener("touchend", this._touchEndHandler, { passive: false });
+  }
+
+  mouseDownHandler(e) {
+    this.mouseDown = true;
+    const rectArea = this.canvas.getBoundingClientRect();
+    this.startX = e.clientX - rectArea.left;
+    this.startY = e.clientY - rectArea.top;
+    this.saved = this.canvas.toDataURL();
+  }
+
+  mouseMoveHandler(e) {
+    if (this.mouseDown) {
+      const rectArea = this.canvas.getBoundingClientRect();
+      const currentX = e.clientX - rectArea.left;
+      const currentY = e.clientY - rectArea.top;
+      this.radius = this.calculateRadius(this.startX, this.startY, currentX, currentY);
+      this.previewCircle(this.startX, this.startY, this.radius);
     }
+  }
 
+  mouseUpHandler() {
+    this.mouseDown = false;
+    this.sendCircle();
+  }
 
-    listen() {
-        // Мышь
-        this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
-        this.canvas.onmousedown = this.mouseDownHandler.bind(this);
-        this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+  touchStartHandler(e) {
+    e.preventDefault();
+    this.mouseDown = true;
+    const rectArea = this.canvas.getBoundingClientRect();
+    this.startX = e.touches[0].clientX - rectArea.left;
+    this.startY = e.touches[0].clientY - rectArea.top;
+    this.saved = this.canvas.toDataURL();
+  }
 
-        // Сенсор
-        this.canvas.addEventListener('touchstart', this.touchStartHandler.bind(this));
-        this.canvas.addEventListener('touchmove', this.touchMoveHandler.bind(this));
-        this.canvas.addEventListener('touchend', this.touchEndHandler.bind(this));
-        this.canvas.addEventListener('touchcancel', this.touchEndHandler.bind(this)); // На всякий случай
+  touchMoveHandler(e) {
+    e.preventDefault();
+    if (this.mouseDown) {
+      const rectArea = this.canvas.getBoundingClientRect();
+      const currentX = e.touches[0].clientX - rectArea.left;
+      const currentY = e.touches[0].clientY - rectArea.top;
+      this.radius = this.calculateRadius(this.startX, this.startY, currentX, currentY);
+      this.previewCircle(this.startX, this.startY, this.radius);
     }
+  }
 
-    // --- Обработчики событий мыши ---
-    mouseUpHandler(e) {
-        this.mouseDown = false;
-        this.sendCircleData();
-        if (this.socket) {
-            this.socket.send(JSON.stringify({
-                method: 'draw',
-                id: this.id,
-                figure: { type: 'finish' }
-            }));
-        }
+  touchEndHandler(e) {
+    e.preventDefault();
+    this.mouseDown = false;
+    this.sendCircle();
+  }
+
+  calculateRadius(startX, startY, currentX, currentY) {
+    const dx = currentX - startX;
+    const dy = currentY - startY;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  previewCircle(x, y, r) {
+    const img = new Image();
+    img.src = this.saved;
+    img.onload = () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, r, 0, 2 * Math.PI);
+      const fillColor = this.fillColor || this.ctx.strokeStyle;
+      this.ctx.fillStyle = fillColor;
+      this.ctx.fill();
+      this.ctx.stroke();
+    };
+  }
+
+  sendCircle() {
+    Circle.staticDraw(
+      this.ctx,
+      this.startX,
+      this.startY,
+      this.radius,
+      this.fillColor,
+      this.ctx.lineWidth,
+      this.ctx.strokeStyle
+    );
+
+    this.ctx.beginPath();
+
+    if (this.socket) {
+      this.socket.send(
+        JSON.stringify({
+          method: "draw",
+          id: this.id,
+          figure: {
+            type: "circle",
+            x: this.startX,
+            y: this.startY,
+            r: this.radius,
+            fillStyle: this.fillColor,
+            lineWidth: this.ctx.lineWidth,
+            strokeStyle: this.ctx.strokeStyle,
+          },
+        })
+      );
+
+      this.socket.send(
+        JSON.stringify({
+          method: "draw",
+          id: this.id,
+          figure: { type: "finish" },
+        })
+      );
     }
-    mouseDownHandler(e) {
-        this.mouseDown = true;
-        this.ctx.beginPath();
-        this.startX = e.pageX - e.target.offsetLeft;
-        this.startY = e.pageY - e.target.offsetTop;
-        this.saved = this.canvas.toDataURL();
-        this.sendStartData();
-    }
+  }
 
-    mouseMoveHandler(e) {
-        if (this.mouseDown) {
-            let currentX = e.pageX - e.target.offsetLeft;
-            let currentY = e.pageY - e.target.offsetTop;
-            this.calculateRadiusAndDraw(currentX, currentY)
-        }
-    }
-
-    // --- Обработчики сенсорных событий ---
-    touchStartHandler(e) {
-        this.mouseDown = true;
-        this.ctx.beginPath();
-        this.startX = e.touches[0].pageX - e.target.offsetLeft;
-        this.startY = e.touches[0].pageY - e.target.offsetTop;
-        this.saved = this.canvas.toDataURL();
-        this.sendStartData();
-    }
-
-    touchMoveHandler(e) {
-        e.preventDefault();
-        if (this.mouseDown) {
-            let currentX = e.touches[0].pageX - e.target.offsetLeft;
-            let currentY = e.touches[0].pageY - e.target.offsetTop;
-            this.calculateRadiusAndDraw(currentX, currentY)
-        }
-    }
-
-
-    touchEndHandler() {
-        this.mouseDown = false;
-        this.sendCircleData();
-        this.socket.send(JSON.stringify({
-            method: 'draw',
-            id: this.id,
-            figure: {
-                type: 'finish',
-            }
-        }))
-    }
-
-    // Общая функция для отправки данных об окружности
-    sendCircleData() {
-        if (this.socket) {
-            this.socket.send(JSON.stringify({
-                method: 'draw',
-                id: this.id,
-                figure: {
-                    type: 'circle',
-                    x: this.startX,
-                    y: this.startY,
-                    r: this.r,
-                    color: this.ctx.fillStyle,
-                    lineWidth: this.ctx.lineWidth,
-                    strokeStyle: this.ctx.strokeStyle
-                }
-            }));
-            this.socket.send(JSON.stringify({
-                method: 'draw',
-                id: this.id,
-                figure: { type: 'finish' }
-            }));
-        }
-
-        Circle.staticDraw(
-            this.ctx,
-            this.startX,
-            this.startY,
-            this.r,
-            this.ctx.fillStyle,
-            this.ctx.lineWidth,
-            this.ctx.strokeStyle
-        );
-    }
-    // Общая функция для отправки данных о начале рисования
-    sendStartData() {
-        if (this.socket) {
-            const lineWidth = this.ctx.lineWidth;
-            const strokeStyle = this.ctx.strokeStyle;
-            this.socket.send(JSON.stringify({
-                method: 'draw',
-                id: this.id,
-                figure: {
-                    type: 'circle',
-                    lineWidth,
-                    strokeStyle,
-                }
-            }));
-        }
-    }
-
-    // Общая функция для расчета радиуса и отрисовки
-    calculateRadiusAndDraw(currentX, currentY) {
-        let width = currentX - this.startX;
-        let height = currentY - this.startY;
-        this.r = Math.sqrt(width ** 2 + height ** 2)
-        this.draw(this.startX, this.startY, this.r)
-    }
-
-    draw(x, y, r) {
-        const img = new Image()
-        img.src = this.saved
-        img.onload = () => {
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
-            this.ctx.beginPath()
-            this.ctx.arc(x, y, r, 0, 2 * Math.PI)
-            this.ctx.fill()
-            this.ctx.stroke()
-        }
-    }
-
-
-    static staticDraw(ctx, x, y, r, lineWidth, strokeStyle, color) {
-        ctx.fillStyle = color;
-        ctx.lineWidth = lineWidth;
-        ctx.strokeStyle = strokeStyle;
-        ctx.beginPath()
-        ctx.arc(x, y, r, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke();
-    }
+  static staticDraw(ctx, x, y, r, fillStyle, lineWidth, strokeStyle) {
+    ctx.fillStyle = fillStyle || strokeStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+  }
 }
