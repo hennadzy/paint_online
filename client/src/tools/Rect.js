25af2b0f04893973d08dde9bff3fa9db7d8af459
hennadz <hennadz@yandex.by>
2025-04-06 11:18:25 +0300

    Локальная отрисовка

--- client/src/tools/Rect.js
+++ client/src/tools/Rect.js
@@ -1,127 +1,143 @@
 import Tool from "./Tool";
 
 export default class Rect extends Tool {
-    constructor(canvas, socket, id) {
-        super(canvas, socket, id);
-        this.mouseDown = false;
-        this.listen();
+  constructor(canvas, socket, id, username) {
+    super(canvas, socket, id, username);
+    this.mouseDown = false;
+    this.destroyEvents();
+    this.listen();
+  }
+
+  listen() {
+    // Обработчики событий для мыши
+    this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+    this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+    this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+
+    // Обработчики сенсорных событий
+    this.canvas.addEventListener("touchstart", this.touchStartHandler.bind(this), { passive: false });
+    this.canvas.addEventListener("touchmove", this.touchMoveHandler.bind(this), { passive: false });
+    this.canvas.addEventListener("touchend", this.touchEndHandler.bind(this), { passive: false });
+  }
+
+  mouseDownHandler(e) {
+    this.mouseDown = true;
+    const rectArea = this.canvas.getBoundingClientRect();
+    this.startX = e.clientX - rectArea.left;
+    this.startY = e.clientY - rectArea.top;
+
+    // Сохраняем текущее состояние холста для превью
+    this.saved = this.canvas.toDataURL();
+  }
+
+  mouseMoveHandler(e) {
+    if (this.mouseDown) {
+      const rectArea = this.canvas.getBoundingClientRect();
+      const currentX = e.clientX - rectArea.left;
+      const currentY = e.clientY - rectArea.top;
+      this.width = currentX - this.startX;
+      this.height = currentY - this.startY;
+      this.previewRect(this.startX, this.startY, this.width, this.height);
     }
+  }
 
-    listen() {
-        this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
-        this.canvas.onmousedown = this.mouseDownHandler.bind(this);
-        this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+  mouseUpHandler() {
+    this.mouseDown = false;
+    this.sendRect();
+  }
 
-        // сенсорные события
-        this.canvas.addEventListener('touchstart', this.touchStartHandler.bind(this), { passive: false });
-        this.canvas.addEventListener('touchmove', this.touchMoveHandler.bind(this), { passive: false });
-        this.canvas.addEventListener('touchend', this.touchEndHandler.bind(this), { passive: false });
+  touchStartHandler(e) {
+    e.preventDefault();
+    this.mouseDown = true;
+    const rectArea = this.canvas.getBoundingClientRect();
+    this.startX = e.touches[0].clientX - rectArea.left;
+    this.startY = e.touches[0].clientY - rectArea.top;
+    this.saved = this.canvas.toDataURL();
+  }
+
+  touchMoveHandler(e) {
+    e.preventDefault();
+    if (this.mouseDown) {
+      const rectArea = this.canvas.getBoundingClientRect();
+      const currentX = e.touches[0].clientX - rectArea.left;
+      const currentY = e.touches[0].clientY - rectArea.top;
+      this.width = currentX - this.startX;
+      this.height = currentY - this.startY;
+      this.previewRect(this.startX, this.startY, this.width, this.height);
     }
+  }
 
-    mouseUpHandler(e) {
-        this.mouseDown = false;
-        this.sendRect();
+  touchEndHandler(e) {
+    e.preventDefault();
+    this.mouseDown = false;
+    this.sendRect();
+  }
+
+  // Отрисовка превью прямоугольника («портрет»)
+  previewRect(x, y, width, height) {
+    const img = new Image();
+    img.src = this.saved;
+    img.onload = () => {
+      // Очищаем холст и восстанавливаем сохранённое изображение
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+      // Отрисовываем прямоугольное превью
+      this.ctx.beginPath();
+      this.ctx.rect(x, y, width, height);
+      this.ctx.fill();
+      this.ctx.stroke();
+    };
+  }
+
+  // Отправка данных окончательного прямоугольника через WebSocket
+  sendRect() {
+    // Финальная отрисовка на локальном холсте
+    Rect.staticDraw(
+      this.ctx,
+      this.startX,
+      this.startY,
+      this.width,
+      this.height,
+      this.ctx.fillStyle,
+      this.ctx.lineWidth,
+      this.ctx.strokeStyle
+    );
+    if (this.socket) {
+      this.socket.send(
+        JSON.stringify({
+          method: "draw",
+          id: this.id,
+          figure: {
+            type: "rect",
+            x: this.startX,
+            y: this.startY,
+            width: this.width,
+            height: this.height,
+            fillStyle: this.ctx.fillStyle,
+            lineWidth: this.ctx.lineWidth,
+            strokeStyle: this.ctx.strokeStyle,
+          },
+        })
+      );
+      // Отправляем сообщение о завершении рисования
+      this.socket.send(
+        JSON.stringify({
+          method: "draw",
+          id: this.id,
+          figure: { type: "finish" },
+        })
+      );
     }
+  }
 
-    mouseDownHandler(e) {
-        this.mouseDown = true;
-        const rect = this.canvas.getBoundingClientRect();
-        this.startX = e.clientX - rect.left;
-        this.startY = e.clientY - rect.top;
-        this.saved = this.canvas.toDataURL();
-    }
-
-    mouseMoveHandler(e) {
-        if (this.mouseDown) {
-            const rect = this.canvas.getBoundingClientRect();
-            const currentX = e.clientX - rect.left;
-            const currentY = e.clientY - rect.top;
-            this.width = currentX - this.startX;
-            this.height = currentY - this.startY;
-            this.draw(this.startX, this.startY, this.width, this.height);
-        }
-    }
-
-    // Сенсорные события
-    touchStartHandler(e) {
-        e.preventDefault();
-        this.mouseDown = true;
-        const rect = this.canvas.getBoundingClientRect();
-        this.startX = e.touches[0].clientX - rect.left;
-        this.startY = e.touches[0].clientY - rect.top;
-        this.saved = this.canvas.toDataURL();
-    }
-
-    touchMoveHandler(e) {
-        e.preventDefault();
-        if (this.mouseDown) {
-            const rect = this.canvas.getBoundingClientRect();
-            const currentX = e.touches[0].clientX - rect.left;
-            const currentY = e.touches[0].clientY - rect.top;
-            this.width = currentX - this.startX;
-            this.height = currentY - this.startY;
-            this.draw(this.startX, this.startY, this.width, this.height);
-        }
-    }
-
-    touchEndHandler(e) {
-        e.preventDefault();
-        this.mouseDown = false;
-        this.sendRect();
-    }
-
-    sendRect() {
-        if (this.socket) {
-            this.socket.send(JSON.stringify({
-                method: 'draw',
-                id: this.id,
-                figure: {
-                    type: 'rect',
-                    x: this.startX,
-                    y: this.startY,
-                    width: this.width,
-                    height: this.height,
-                    color: this.ctx.fillStyle,
-                    lineWidth: this.ctx.lineWidth,
-                    strokeStyle: this.ctx.strokeStyle,
-                }
-            }));
-            this.socket.send(JSON.stringify({ method: 'draw', id: this.id, figure: { type: 'finish' } }));
-        }
-
-        // обязательно тут же самая локальная отрисовка
-        Rect.staticDraw(
-            this.ctx,
-            this.startX,
-            this.startY,
-            this.width,
-            this.height,
-            this.ctx.fillStyle,
-            this.ctx.lineWidth,
-            this.ctx.strokeStyle
-        );
-    }
-
-    draw(x, y, w, h) {
-        const img = new Image();
-        img.src = this.saved;
-        img.onload = () => {
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
-            this.ctx.beginPath();
-            this.ctx.rect(x, y, w, h);
-            this.ctx.fill();
-            this.ctx.stroke();
-        };
-    }
-
-    static staticDraw(ctx, x, y, w, h, lineWidth, strokeStyle, color) {
-        ctx.fillStyle = color;
-        ctx.lineWidth = lineWidth;
-        ctx.strokeStyle = strokeStyle;
-        ctx.beginPath();
-        ctx.rect(x, y, w, h);
-        ctx.fill();
-        ctx.stroke();
-    }
+  // Статический метод для окончательной отрисовки прямоугольника
+  static staticDraw(ctx, x, y, width, height, fillStyle, lineWidth, strokeStyle) {
+    ctx.fillStyle = fillStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.fill();
+    ctx.stroke();
+  }
 }
