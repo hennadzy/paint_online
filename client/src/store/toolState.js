db1154adc50773ea926649b7676b2a5549876194
hennadz <hennadz@yandex.by>
2025-09-30 00:21:54 +0300

    Исправление локальности цвета инструмента

--- client/src/store/toolState.js
+++ client/src/store/toolState.js
@@ -47,12 +47,12 @@ class ToolState {
     }
   }
 
-  setStrokeColor(color) {
-    this.strokeColor = color; // ✅ сохраняем цвет
-    if (this.tool?.setStrokeColor) {
-      this.tool.setStrokeColor(color);
-    }
-  }
+//   setStrokeColor(color) {
+//     this.strokeColor = color; // ✅ сохраняем цвет
+//     if (this.tool?.setStrokeColor) {
+//       this.tool.setStrokeColor(color);
+//     }
+//   }
 
 
   setFillColor(color) {
