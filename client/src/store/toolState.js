4b5851c48b236a06c0ce622f066b3ec634a07573
hennadz <hennadz@yandex.by>
2025-09-30 20:56:43 +0300

    Запоминаем толщину ползунка

--- client/src/store/toolState.js
+++ client/src/store/toolState.js
@@ -1,71 +1,81 @@
 import { makeAutoObservable, observable } from "mobx";
 
 class ToolState {
-  tool = null;
+    tool = null;
 
-  strokeColor = "#000000";
-  fillColor = "#000000";
+    strokeColor = "#000000";
+    fillColor = "#000000";
 
-  lineWidths = {
-    brush: 1,
-    rect: 1,
-    circle: 1,
-    eraser: 10,
-    line: 1
-  };
+    lineWidths = {
+        brush: 1,
+        rect: 1,
+        circle: 1,
+        eraser: 10,
+        line: 1
+    };
 
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  setTool(tool) {
-    if (this.tool?.restorePreviousColors) {
-      this.tool.restorePreviousColors();
+    constructor() {
+        makeAutoObservable(this);
     }
 
-    if (this.tool?.destroyEvents) {
-      this.tool.destroyEvents();
+    setTool(tool) {
+        if (this.tool?.restorePreviousColors) {
+            this.tool.restorePreviousColors();
+        }
+
+        if (this.tool?.destroyEvents) {
+            this.tool.destroyEvents();
+        }
+
+        const toolName = tool.constructor.name.toLowerCase();
+        const savedWidth = this.lineWidths[toolName] ?? 1;
+        tool.lineWidth = savedWidth;
+
+        if (tool.setStrokeColor) {
+            tool.setStrokeColor(this.strokeColor);
+        }
+        if (tool.setFillColor) {
+            tool.setFillColor(this.fillColor);
+        }
+
+        this.canvas.onmousedown = null;
+        this.canvas.onmousemove = null;
+        this.canvas.onmouseup = null;
+        this.canvas.ontouchstart = null;
+        this.canvas.ontouchmove = null;
+        this.canvas.ontouchend = null;
+
+        this.tool = observable(tool);
+
+        if (this.tool.listen) {
+            this.tool.listen();
+        }
+
+
+
     }
 
-    const toolName = tool.constructor.name.toLowerCase();
-    const savedWidth = this.lineWidths[toolName] ?? 1;
-    tool.lineWidth = savedWidth;
-
-    if (tool.setStrokeColor) {
-      tool.setStrokeColor(this.strokeColor);
-    }
-    if (tool.setFillColor) {
-      tool.setFillColor(this.fillColor);
+    setStrokeColor(color) {
+        this.strokeColor = color;
+        if (this.tool?.setStrokeColor) {
+            this.tool.setStrokeColor(color);
+        }
     }
 
-     this.tool = observable(tool);
-
-    if (this.tool.listen) {
-      this.tool.listen();
+    setFillColor(color) {
+        this.fillColor = color;
+        if (this.tool?.setFillColor) {
+            this.tool.setFillColor(color);
+        }
     }
-  }
 
-  setStrokeColor(color) {
-    this.strokeColor = color;
-    if (this.tool?.setStrokeColor) {
-      this.tool.setStrokeColor(color);
+    setLineWidth(lineWidth) {
+        if (this.tool) {
+            const toolName = this.tool.constructor.name.toLowerCase();
+            this.tool.lineWidth = lineWidth;
+            this.lineWidths[toolName] = lineWidth;
+        }
     }
-  }
-
-  setFillColor(color) {
-    this.fillColor = color;
-    if (this.tool?.setFillColor) {
-      this.tool.setFillColor(color);
-    }
-  }
-
-  setLineWidth(lineWidth) {
-    if (this.tool) {
-      const toolName = this.tool.constructor.name.toLowerCase();
-      this.tool.lineWidth = lineWidth;
-      this.lineWidths[toolName] = lineWidth;
-    }
-  }
 }
 
 export default new ToolState();
