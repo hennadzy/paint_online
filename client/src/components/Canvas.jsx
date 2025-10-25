6b0926e4c032375d2b3e900d145cd9508013bad5
hennadz <hennadz@yandex.by>
2025-10-26 00:20:56 +0300

    Исправление работы инструментов за пределами холста

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -243,7 +243,7 @@ const Canvas = observer(() => {
       {!isRoomCreated && (
         <Button
           variant="primary"
-          onPointerUp={handleCreateRoomClick}
+          onClick={handleCreateRoomClick}
           style={{ marginTop: "10px" }}
         >
           Создать комнату
