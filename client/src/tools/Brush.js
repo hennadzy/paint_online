618179ed39dd2d4cc78db459a8e3632ab4d90a69
hennadz <hennadz@yandex.by>
2025-10-27 21:15:38 +0300

    Ннастройки прозрачности для инструментов

--- client/src/tools/Brush.js
+++ client/src/tools/Brush.js
@@ -24,6 +24,7 @@ export default class Brush extends Tool {
 
   setStrokeOpacity(opacity) {
     this.strokeOpacity = opacity;
+    this.strokeColor = this.hexToRgba(this.strokeStyle, opacity);
   }
 
   setLineWidth(width) {
