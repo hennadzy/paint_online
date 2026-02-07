007682cce8ac7d287730089e267be9782806878a
hennadz <hennadz@yandex.by>
2026-02-07 20:00:35 +0300

    Фикс мобильных

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -493,7 +493,7 @@ const Canvas = observer(() => {
 
           scrollbar.style.left = `${rect.left}px`;
           scrollbar.style.width = `${rect.width - TRACK_HORIZONTAL_INSET}px`;
-          scrollbar.style.top = `${rect.bottom - 20}px`;
+          scrollbar.style.top = `${Math.min(rect.bottom - 20, window.innerHeight - 20)}px`;
           scrollbar.style.height = '20px';
 
           if (hasScroll) {
