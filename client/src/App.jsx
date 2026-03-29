3eee2e2bb0947cc1c6e5878ac401fa65fb112398
hennadz <hennadz@yandex.by>
2026-03-29 19:45:17 +0300

    фикс Профиля

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import AuthPage from "./components/AuthPage";
 import AdminPage from "./components/AdminPage";
 import ColoringPage from "./components/ColoringPage";
 import GalleryPage from "./components/GalleryPage";
+import RoomInterface from "./components/RoomInterface";
 import { Routes, Route, useLocation, useParams, useNavigate, Navigate } from 'react-router-dom';
 import canvasState from "./store/canvasState";
 import userState from "./store/userState";
@@ -107,6 +108,7 @@ const App = observer(() => {
                     <Route path='/:id' element={<RoomRoute />} />
                     <Route path='*' element={<Navigate to="/404" replace />} />
                 </Routes>
+                {canvasState.showRoomInterface && <RoomInterface roomId={null} />}
             </div>
         </div>
     );
