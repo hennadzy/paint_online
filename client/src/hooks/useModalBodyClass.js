22448ae09fe53841066eddc641f91f9e79e43eb2
hennadz <hennadz@yandex.by>
2026-04-18 12:33:50 +0300

    Фикс окна вост раб

--- client/src/hooks/useModalBodyClass.js
+++ client/src/hooks/useModalBodyClass.js
@@ -4,12 +4,12 @@ import canvasState from '../store/canvasState';
 export function useModalBodyClass() {
   useEffect(() => {
     const isModalOpen = canvasState.showAboutModal || canvasState.showRoomInterface ||
-      canvasState.modalOpen || canvasState.showRestoreDialog;
+      canvasState.modalOpen || canvasState.showRestoreDialog || canvasState.showInactiveModal;
     if (isModalOpen) {
       document.body.classList.add('modal-open');
     } else {
       document.body.classList.remove('modal-open');
     }
     return () => document.body.classList.remove('modal-open');
-  }, [canvasState.showAboutModal, canvasState.showRoomInterface, canvasState.modalOpen, canvasState.showRestoreDialog]);
+  }, [canvasState.showAboutModal, canvasState.showRoomInterface, canvasState.modalOpen, canvasState.showRestoreDialog, canvasState.showInactiveModal]);
 }
