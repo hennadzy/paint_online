300908eb63ef6e0e166dbcc59af57fda51e5d376
hennadz <hennadz@yandex.by>
2026-05-30 22:08:43 +0300

    СЕО

--- server/index.js
+++ server/index.js
@@ -288,6 +288,7 @@ app.get('/sitemap.xml', async (req, res) => {
     let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<!-- DYNAMIC SITEMAP - Generated 2026-01-17 -->
 
   <url>
     <loc>${baseUrl}/</loc>
