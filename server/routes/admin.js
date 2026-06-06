4431268e90607fa5008f05d67fb33b5837ca448a
hennadz <hennadz@yandex.by>
2026-06-06 13:34:44 +0300

    Разделы раскрасок фикс3

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -22,6 +22,7 @@ const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-
 
 const { generateSlug, ensureUniqueColoringPageSlug, ensureUniqueColoringSectionSlug } = require('../utils/slug');
 
+const router = express.Router();
 
 const coloringUpload = multer({
   storage: multer.memoryStorage(),
